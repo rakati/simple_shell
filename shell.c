56de48e10f2b401af6fcfe5c1d14e4e615ff5bed
rakati <ouhaddounouredine@gmail.com>
2023-12-06 20:22:46 +0100

    init starter of our shell

---
+++ shell.c
@@ -0,0 +1,15 @@
+#include "shell.h"
+
+/**
+ * main - Entry point for our shell program
+ *
+ * @ac: (integer) number of argument of the program
+ * @av: (array of strings) array of parameters of the program with len=ac
+ * @envp: (array of strings) array of environment variable path.
+ * @return int 0 for success otherwise error number
+ */
+int main(int ac, char **av, char **envp)
+{
+
+	return (0);
+}
