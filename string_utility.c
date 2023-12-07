032672eaa4f0f8ebdcaf935a4819d6f7b0f8dcbc
rakati <ouhaddounouredine@gmail.com>
2023-12-07 16:23:22 +0100

    add puts function, our function doesn't print newline at the end

--- string_utility.c
+++ string_utility.c
@@ -17,3 +17,20 @@ int _strlen(char *s)
 		i++;
 	return (i);
 }
+
+/**
+ * _puts - print string to stdout at once.
+ *
+ * @s: array of characters
+ * Return: Number of character printed, otherwise -1 if an error occurred
+ */
+
+void _puts(char *s)
+{
+	int len;
+
+	len = _strlen(s);
+	if (len >= 0)
+		len = write(1, s, len);
+	return (len);
+}
