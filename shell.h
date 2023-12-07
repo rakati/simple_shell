0c683c2af744f02541cd1f55ea568ea67620e55c
rakati <ouhaddounouredine@gmail.com>
2023-12-07 16:22:54 +0100

    Add getline function signature

--- shell.h
+++ shell.h
@@ -3,4 +3,23 @@
 
 #include <unistd.h>
 
+/**
+ * String Utility functions
+ */
+int _strlen(char *s);
+void _puts(char *s);
+int _getline(const int fd, char **line)
+
+/**
+ * Parser functions
+ */
+
+/**
+ * execute functions
+ */
+
+/**
+ * Implemented Builtin functions
+ */
+
 #endif
