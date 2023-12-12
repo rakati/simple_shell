6e2dc8c522c0efc4c59c61ff31b9492a83bb4cfc
amine-zzr <aminezoukri@gmail.com>
2023-12-12 01:21:58 +0000

    _strchr prototype modification

--- shell.h
+++ shell.h
@@ -9,12 +9,11 @@
 #include <fcntl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <fcntl.h>
 
 int _getline(char **line, const int fd);
 int _read(int fd, char **rd, char *buff);
 void process_line(char **line, char **rd, int pos);
-char *_strchr(char *s, char c);
+int _strchr(char *s, char c);
 char *_strncpy(char *dest, char *src, int n);
 char *_strdup(char *str);
 char *_strcat(char *dest, char *src);
