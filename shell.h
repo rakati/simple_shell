481c93313f37551a0436624afb8ba5c8b3e9dc3a
rakati <ouhaddounouredine@gmail.com>
2023-12-12 21:47:32 +0100

    Solve conflict and re-order declarations in header

--- shell.h
+++ shell.h
@@ -10,14 +10,79 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 
+/*
+ * getline function
+ */
 int _getline(char **line, const int fd);
 int _read(int fd, char **rd, char *buff);
-int _strchr(char *s, char c);
-char *_strncpy(char *dest, char *src, int n);
+
+/*
+ * Utility functions
+ */
+int _index(char *s, char c);
+
+/*
+ * String Utility functions
+ */
 char *_strdup(char *str);
+char *_strncpy(char *dest, char *src, int n);
+int _strlen(char *);
+void _puts(char *);
 char *_strcat(char *dest, char *src);
-int _strlen(char *s);
+
+/*
+ * Parser functions
+ */
 char *_strtok(char *str, const char *delim);
 char *_strpbrk(char *s, const char *accept);
+
+/**
+ * enum rel - enum for type of relation between commands in a single line
+ * @OR: indicating relation with next command is '||' operator.
+ * @AND: indicating relation with next command is '&&' operator.
+ * @SEM: indicating relation with next command is ';' operator.
+ * @END: means no command after current command.
+ */
+enum rel
+{
+	OR,
+	AND,
+	SEM,
+	END
+};
+
+/**
+ * struct cmd_s - singly linked list of commands
+ * @cmd: NULL terminated array of strings - hold the cmd in index 0, and it's
+ * arguments starting from index 1.
+ * @type: enum rel - indicate the relationship between commands if there are
+ * other commands, the rel can be '&&', ';' or ||.
+ * @next: points to the next node
+ *
+ * Description: this data structure store info about parsed line, that can have
+ * one or multiple commands related with each other by '&&', ';' or '||'.
+ * Example:
+ * ========
+ * simple_cmd = "ls -l"
+ * => {cmd=["/bin/ls", "-l"], type=END, next=NULL};
+ *
+ * two_cmd = "ls; cat filename"
+ * => {cmd=["/bin/ls"], type=SEM, next=ptr_next}
+ *		=> {cmd=["/bin/cat", "file_name"], type=END, next=NULL};
+ */
+typedef struct cmd_s
+{
+	char **cmd;
+	enum rel type;
+	struct cmd_s *next;
+} t_cmd;
+
+/*
+ * execute functions
+ */
+
+/*
+ * Implemented Builtin functions
+ */
 
 #endif
