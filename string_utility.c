c5b8fba015564a6cb4291bceaa0da3f08b6f4f89
amine-zzr <aminezoukri@gmail.com>
2023-12-13 20:20:10 +0000

    _strcmp function added to compare strings

--- string_utility.c
+++ string_utility.c
@@ -34,3 +34,26 @@ void _puts(char *s)
 		len = write(1, s, len);
 	return (len);
 }
+
+/**
+ * _strcmp - compare two string
+ * @s1: input string
+ * @s2: input string
+ *
+ * Return: s1[i] - s2[i]
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+		i++;
+	}
+	return (0);
+}
