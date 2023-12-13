d8da720153d632988aca7ea2699a790b91080d47
rakati <ouhaddounouredine@gmail.com>
2023-12-13 20:44:30 +0100

    implement realloc function

--- general_utility.c
+++ general_utility.c
@@ -17,3 +17,27 @@ int _index(const char *s, const char c)
 			return (i);
 	return (-1);
 }
+
+/**
+ * _realloc - resize of the memory block pointed to by ptr to size.
+ * The contents will be unchanged in the range from the start of the region
+ * up to the minimum of the old and new sizes.
+ * @ptr: void ptr - pointer to memory bloc to resize.
+ * @size: positive integer - the new memory size for the ptr.
+ * Return: pointer to the new memory resized or NULL if an error occurred.
+ */
+void *_realloc(void *ptr, size_t size)
+{
+	char *new_ptr, *p;
+	size_t i;
+
+	p = (char *)ptr;
+	new_ptr = size == 0 ? NULL : malloc(size);
+	if (ptr == NULL)
+		return (new_ptr);
+	for (i = 0; p[i] && i < size; i++)
+		new_ptr[i] = p[i];
+	new_ptr[i] = p[i];
+	free(ptr);
+	return (new_ptr);
+}
