955acffe02409b81a82403e100a2e987cfba1c43
rakati <ouhaddounouredine@gmail.com>
2023-12-21 15:54:29 +0100

    fix bug strncmp

--- string_utility2.c
+++ string_utility2.c
@@ -66,7 +66,7 @@ int _strncmp(char *s1, char *s2, size_t n)
 {
 	size_t i = 0;
 
-	while (s1[i] != '\0' && s2[i] != '\0' && i < n)
+	while (s1[i] != '\0' && s2[i] != '\0' && i < n - 1)
 	{
 		if (s1[i] != s2[i])
 		{
@@ -74,7 +74,6 @@ int _strncmp(char *s1, char *s2, size_t n)
 		}
 		i++;
 	}
-
 	return (s1[i] - s2[i]);
 }
 
